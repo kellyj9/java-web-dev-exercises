0af7ade185cb8145a560fee2f2bc13aa83acc75d
Kelly Jones <kellymj6@gmail.com>
2021-10-04 12:53:04 -0400

    completed LC101 lesson exercises about Java Classes for part 1 and part 2

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -1,7 +1,19 @@
 package org.launchcode.java.demos.lsn3classes1;
 
 public class SchoolPractice {
+
     public static void main(String[] args) {
-        // Instantiate your Student class for part 2 here!
+        org.launchcode.java.demos.lsn3classes1.Student kelly = new org.launchcode.java.demos.lsn3classes1.Student("Kelly", 1, 1, 4.0);
+        System.out.println(kelly.getName() + " is a student!");
+        System.out.println(kelly);
+        System.out.println("After adding 12 course credits with a grade of 3.5:");
+        kelly.addGrade(12, 3.5);
+        System.out.println(kelly);
+        System.out.println("After adding 25 course credits with a grade of 3.8:");
+        kelly.addGrade(25, 3.8);
+        System.out.println(kelly);
+        System.out.println("After adding 60 course credits with a grade of 3.9:");
+        kelly.addGrade(60, 3.9);
+        System.out.println(kelly);
     }
 }
--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -10,4 +10,74 @@ public class Student {
     private int numberOfCredits = 0;
     private double gpa = 0.0;
 
+    public Student(String name, int studentId, int numberOfCredits, double gpa) {
+        this.name = name;
+        this.studentId = studentId;
+        this.numberOfCredits = numberOfCredits;
+        this.gpa = gpa;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public int getNumberOfCredits() {
+        return numberOfCredits;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public void setNumberOfCredits(int numberOfCredits) {
+        this.numberOfCredits = numberOfCredits;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    // This method accepts two parameters—a number of course credits and
+    // a numerical grade (0.0-4.0). With this data, the student’s GPA is updated.
+    public void addGrade (int courseCredits, double grade) {
+        double totalQualityScore = this.gpa * this.numberOfCredits;
+        totalQualityScore += courseCredits * grade;
+        this.numberOfCredits += courseCredits;
+        this.gpa = totalQualityScore / this.numberOfCredits;
+    }
+
+    // returns the student’s level based on the number of credits they have earne
+    public String getGradeLevel(int credits) {
+        if (credits <= 29){
+            return "freshman";
+        } else if (credits <= 59){
+            return "sophomore";
+        } else if (credits <= 89) {
+            return "junior";
+        } else {
+            return "senior";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s is a %s with %d credits and a GPA of %.2f.",
+                this.name,
+                this.getGradeLevel(this.numberOfCredits),
+                this.getNumberOfCredits(),
+                this.getGpa());// also testing get functions
+
+    }
 }
